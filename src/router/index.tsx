012d9a46951a72312927238a3e6d32f88668bc76
fumk <fumk@paraview.cn>
2020-05-12 15:55:28 +0800

    router懒加载

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,22 @@
-import React,{FunctionComponent} from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from "../views/home";
+import React, {FunctionComponent, lazy, Suspense} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Loading from "../components/loading";
+
+//router懒加载
+const lazyImport = (path: string) => lazy(() => import(`../views/${path}`));
 
 interface OwnProps {
 }
+
 type Props = OwnProps;
 
 const DynamicRouter: FunctionComponent<Props> = () => {
     return (
-        <Switch>
-            <Route path='/' component={Home}/>
-        </Switch>
+        <Suspense fallback={Loading}>
+            <Switch>
+                <Route exact path='/' component={lazyImport('home/index')}/>
+            </Switch>
+        </Suspense>
     );
 };
 
